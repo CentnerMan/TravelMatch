9036af0898cd1eb568ec12eaea973fc51aecd121
DiNikOs <demon22odn08@rambler.ru>
2020-06-11 23:39:41 +0300

    dev_week1. ProfileGetDto

---
+++ src/main/java/ru/travelmatch/services/UserProfileServiceImpl.java
@@ -0,0 +1,16 @@
+/**
+ * @author Ostrovskiy Dmitriy
+ * @created 11.06.2020
+ * UserProfileServiceImpl
+ * @version v1.0
+ */
+
+package ru.travelmatch.services;
+
+import ru.travelmatch.base.entities.User;
+import ru.travelmatch.dto.ProfileGetDto;
+
+public class UserProfileServiceImpl implements UserProfileService {
+
+    private ProfileGetDto profileGetDto;
+}
